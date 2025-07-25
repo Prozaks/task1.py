bae82e4381085fa9e8d3cfcd0f1988519167ef98
yilkimezakka@gmail.com <yilkimezakka@gmail.com>
2025-07-25 06:46:22 +0000

    task3 commit

--- task3.py
+++ task3.py
@@ -10,3 +10,13 @@ money = [1000, 1200, 800, 1500, 1100]
 
 → Perform the corrections and computations, and print all results.
 """
+
+money = [1000, 1200, 800, 1500, 1100]
+
+sum = money[0] + money[1] + money[2] + money[3] + money[4]
+print(sum)
+
+money[2] = ("1000")
+print(money)
+
+print(money[::-1])
